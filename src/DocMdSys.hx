77a43d825a5abb979f6de89d9e918114379a871b
YellowAfterlife <yellowafterlife@hotmail.com>
2023-02-26 14:35:12 +0200

    Purge an empty "uncategorized" section in dmdAuto

--- src/DocMdSys.hx
+++ src/DocMdSys.hx
@@ -164,10 +164,11 @@ class DocMdSys {
 		html = ~/<p>\s*<\/p>/g.replace(html, "");
 		html = ~/(\r?\n[ \t]*)(\r?\n[ \t]*)(<\/p><p>)/g.replace(html, "$1$3$2");
 		if (DocMd.genMode == Nested) {
-			html = ~/<div class="item">(<span class="header">.+?<\/span>)<div class="content"><\/div><\/div>/g
-				.replace(html, '<div class="item empty">$1</div>');
-			html = ~/<div class="item">(<a class="header"[^>]*>.+?<\/a>)<div class="content"><\/div><\/div>/g
-				.replace(html, '<div class="item empty">$1</div>');
+			// fold empty sections:
+			html = ~/<section>(<header.*?>.+?<\/header>)<article><\/article><\/section>/g
+				.replace(html, '<section class="empty">$1</section>');
+			// get rid of empty Uncategorized section in particular:
+			html = html.replace('<section class="empty"><header id="uncategorized"><a href="#uncategorized" title="(permalink)">Uncategorized</a></header></section>', '');
 		}
 		function preproc(html:String, depth:Int = 0):String {
 			// <!--%[if 
